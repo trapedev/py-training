75133f022df1d88d0c1c2570175b2d7a6131fc66
KeiTaylor0606 <mvemtae5757@outlook.jp>
2021-09-26 18:22:07 +0900

    アノテーションと文書化文字列付き関数

---
+++ grammer/function/DocumentedStringsAndAnnotations/29.py
@@ -0,0 +1,16 @@
+"""アノテーションと文書化文字列付き関数"""
+
+def puts(n: int, s: str) -> None:
+    """n個のsを連続して表示
+
+    仮引数:
+        n -- 表示する文字列の個数
+        s -- 表示する文字列
+    返却値:
+        無し
+    
+    """
+    for _ in range(n):
+        print(s, end='')
+
+print(puts.__doc__)
