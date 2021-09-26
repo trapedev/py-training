2921f472cadd91e1128ceb4822d4ffb9b38a6e86
KeiTaylor0606 <mvemtae5757@outlook.jp>
2021-09-26 19:59:50 +0900

    min_max2モジュールのmin_max2関数を呼び出す

---
+++ grammer/ModuleAndPackage/Module/2.py
@@ -0,0 +1,9 @@
+"""min_max2モジュールのmin_max2関数を呼び出す"""
+
+import example_1
+x = float(input('x = '))
+y = float(input('y = '))
+
+mini, maxi = example_1.min_max2(x, y)
+print('min = ', mini)
+print('max = ', maxi)
